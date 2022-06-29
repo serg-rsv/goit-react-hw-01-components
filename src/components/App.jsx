c7c580c7c6f9e85026f4bac53d264d1eeeb41f00
Serhii Rybak <rybaksw@gmail.com>
2022-06-29 18:29:55 +0300

    Task03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 import user from '../json/user.json';
 import data from '../json/data.json';
+import friends from '../json/friends.json';
 
 export const App = () => {
   return (
@@ -14,6 +16,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,28 @@
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ id, avatar, name, isOnline }) => {
+        return (
+          <FriendListItem
+            key={id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
