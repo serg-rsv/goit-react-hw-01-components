235a21f567a08efb7c4bfea35492214ad50b6ab1
Serhii Rybak <rybaksw@gmail.com>
2022-06-29 22:45:22 +0300

    task04

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import user from '../json/user.json';
 import data from '../json/data.json';
 import friends from '../json/friends.json';
+import transactions from '../json/transactions.json';
 
 export const App = () => {
   return (
@@ -17,6 +19,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
