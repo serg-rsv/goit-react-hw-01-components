7b8c0ce934b26706f9e413a3a5f088929b4cdc3e
Serhii Rybak <rybaksw@gmail.com>
2022-06-29 16:58:36 +0300

    task02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 import user from '../json/user.json';
+import data from '../json/data.json';
 
 export const App = () => {
   return (
@@ -11,6 +13,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
